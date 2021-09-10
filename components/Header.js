e52f3028a9a9b75722bf81d0221fe0be05304fc1
moh-ash96 <moh.ash96@gmail.com>
2021-09-10 10:59:58 +0300

    made the header

--- components/Header.js
+++ components/Header.js
@@ -6,8 +6,26 @@ import Image from 'next/image';
 export default (
     function Header(props){
         return(
-            <header>
+            <header className="flex flex-row justify-between p-6">
+                <div>
                 <Image src={logo} alt='logo' />
+                </div>
+                <nav>
+                    <ul className="flex flex-row">
+                        <li className="mr-14">
+                            <a href="#">Home</a>
+                        </li>
+                        <li className="mr-14">
+                            <a href="#">Products</a>
+                        </li>
+                        <li className="mr-14">
+                            <a href="#">About Us</a>
+                        </li>
+                        <li className="mr-14">
+                            <a href="https://foodsto-api.herokuapp.com/admin/">Admin Login</a>
+                        </li>
+                    </ul>
+                </nav>
             </header>
         )
     }
